cbf2f6c2d2c2edfeaf7c04785ad182139148c9d8
Oluwaseyi <hassanabdullahioluwaseyi@gmail.com>
2025-01-03 22:34:04 +0100

    changed baseURL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:2007/api",
+    baseURL: "https://dreyxink-server.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     }
